9d66ebb7c117dee3cf1a6b63d38849aa48e413ee
mcmadan <149359725+mcmadan@users.noreply.github.com>
2025-05-01 15:23:35 -0300

    watchlist.py

---
+++ watchlist.py
@@ -0,0 +1,11 @@
+# watchlist.py
+
+watch_addresses = {
+    "binance_cold_wallet": "0x123...abc",
+    "market_maker_xyz": "0x456...def",
+    "vc_famoso": "0x789...ghi"
+}
+
+watched_tokens = [
+    "USDT", "ETH", "ARB", "OP", "TOKEN_X"
+]
