33cc720749de99ec523b3a5a2ffc1a13108e54ca
mcmadan <149359725+mcmadan@users.noreply.github.com>
2025-05-01 15:29:13 -0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
-import logging
+# bot.py
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import Message
 from aiogram.utils import executor
+import logging
+import asyncio
 import os
+from monitor import monitor_wallets
 
-API_TOKEN = os.getenv("BOT_TOKEN")
+API_TOKEN = os.getenv("BOT_TOKEN", "8069388714:AAH2PjzKD-D15E8mxbDmeof3dHIsxrZuKjs")
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
@@ -14,5 +18,12 @@ dp = Dispatcher(bot)
 async def send_welcome(message: Message):
     await message.reply("Inteligência ativada")
 
+@dp.message_handler(commands=["status"])
+async def status(message: Message):
+    await message.reply("Apollo 1.5 está online e monitorando...")
+
+async def on_startup(_):
+    asyncio.create_task(monitor_wallets())
+
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
